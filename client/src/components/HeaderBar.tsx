addd9caceab652c17bc03728f24753484de13773
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2023-07-10 22:03:12 +0100

    added fontawesome icons (free)

--- client/src/components/HeaderBar.tsx
+++ client/src/components/HeaderBar.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Link from "@mui/material/Link";
 import "./HeaderBar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 const HeaderBar: React.FC = (): JSX.Element => {
   return (
     <>
+      <FontAwesomeIcon icon={icon({ name: "user-secret" })} />
       <header>
         <Link href="/">Home</Link>
       </header>
