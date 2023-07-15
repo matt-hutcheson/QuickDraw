4609bab71e62fe84175da6d1866d29de11c73dc4
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2023-07-15 17:40:54 +0100

    added alias of @ for imports

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import macrosPlugin from "vite-plugin-babel-macros";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), macrosPlugin()],
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "./src") },
+  },
 });
