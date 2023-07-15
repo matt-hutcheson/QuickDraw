bd7c721269929f2b6dfd6777d046c9ffc9d4e301
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2023-07-15 17:42:23 +0100

    Header bar refactored to use mui components, and have light/dark mode

--- client/src/components/HeaderBar.tsx
+++ client/src/components/HeaderBar.tsx
@@ -1,17 +1,47 @@
-import React from "react";
-import Link from "@mui/material/Link";
+import React, { useContext } from "react";
+import { Box, IconButton, useTheme } from "@mui/material";
+import InputBase from "@mui/material/InputBase";
+import {
+  DarkModeOutlined as DarkModeOutlinedIcon,
+  LightModeOutlined as LightModeOutlinedIcon,
+  Search as SearchIcon,
+} from "@mui/icons-material";
+import { ColorModeContext, tokens } from "@/theme";
 import "./HeaderBar.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 const HeaderBar: React.FC = (): JSX.Element => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
   return (
-    <>
-      <FontAwesomeIcon icon={icon({ name: "user-secret" })} />
-      <header>
-        <Link href="/">Home</Link>
-      </header>
-    </>
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      p={2}
+      className="header-bar"
+    >
+      <Box
+        display="flex"
+        sx={{ backgroundColor: colors.primary[400] }}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+      </Box>
+    </Box>
   );
 };
 
